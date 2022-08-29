a0ed4784c4999da62e715a50c5131ede74cd4402
GabilyAslanov <aslanov.gabily@gmail.com>
2022-08-29 22:07:32 +0300

    Seminar4
    Seminar4

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -6,17 +6,129 @@ namespace Seminar2
     {
         static void Main(string[] args)
         {
+            //Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+            int num = new Random().Next(1, 100000);
+            // ViewSecondNumber(num);
+
+            /*конец*/
+
+            //Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+            System.Console.Write("Введите число: ");
+            num = int.Parse(Console.ReadLine());
+
+            System.Console.Write("Введите индекс: ");
+            int numIndex = int.Parse(Console.ReadLine());
+
+            ViewaThirdNumber1(num, numIndex);
+            /*конец*/
+
+            //Задача 15: Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+            // System.Console.Write("Введите день недели: ");
+            // int dayWeek = int.Parse(Console.ReadLine());
+
+            // DayOffWeek(dayWeek);
+
+            /*конец*/
+
             // 9.1 Напишите программу, которая выводит случайное число из отрезка [10, 999] и показывает наибольшую цифру числа.
-            MaxNumber();
+            // MaxNumber();
 
             // 9.2 Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
-            MaxNumber2();
+            // MaxNumber2();
 
             //11. Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
-            RemoveMiddleNumber();
+            //RemoveMiddleNumber();
 
             // 12. Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
-            SecondNumberMultipleFirst();
+            // SecondNumberMultipleFirst();
+        }
+
+
+        //Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+        static void ViewSecondNumber(int num)
+        {
+            if (num > 99 && num < 1000)
+            {
+                int secondNumber = (num / 10) % 10; //789/10= 78 %10=8
+
+                Console.WriteLine($"Вторая цифра в числе {num} -> {secondNumber}");
+            }
+            else
+                Console.WriteLine($"Число {num} не трёхзначное");
+        }
+
+        //Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+        static void ViewaThirdNumber1(int num, int index)
+        {
+            if (num > 99)
+            {
+                //количество цифр в числе
+                int length = (int)Math.Log10(num);
+//456789
+                //Дилитель числа на какое число мы должны поделить ,для того, чтобы получить число  
+                int numberPow = (int)Math.Pow(10, length - index);
+//10^6-3=1000
+                //мы получаем нашу цифру
+                int result = num / numberPow % 10;
+//456789/1000= 456 %10=6
+                Console.WriteLine($"{index} цифра в числе {num} -> {result}");
+            }
+            else
+                Console.WriteLine($"У числа нет трёх цифр");
+        }
+
+        //Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+        //То что дала GeekBrains
+        //645 -> 5
+
+        // 78 -> третьей цифры нет
+
+        // 32679 -> 6
+        static void ViewaThirdNumber2(int num, int index = 3)
+        {
+            Console.Clear();
+
+            if (num < 100)
+            {
+                Console.WriteLine($"Третьей цифры нет");
+                return;
+            }
+            int thirdNumber = (num / 100) % 10;
+            Console.WriteLine($"Третья цифра числа {num} -> {thirdNumber}");
+        }
+
+        //Задача 15: Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+        static void DayOffWeek(int dayWeek)
+        {
+            switch (dayWeek)
+            {
+                case 1:
+                    Console.WriteLine("Понедельник- НЕТ");
+                    break;
+                case 2:
+                    Console.WriteLine("Вторник- НЕТ");
+                    break;
+                case 3:
+                    Console.WriteLine("Среда- НЕТ");
+                    break;
+                case 4:
+                    Console.WriteLine("Четверг- НЕТ");
+                    break;
+                case 5:
+                    Console.WriteLine("Пятница- Да");
+                    break;
+                case 6:
+                    Console.WriteLine("Суббота- Нет");
+                    break;
+                case 7:
+                    Console.WriteLine("Воскресенье- Нет");
+                    break;
+                default:
+                    Console.WriteLine("Некорректный номер дня недели");
+                    break;
+            }
         }
 
         // 9. Напишите программу, которая выводит случайное число из отрезка [10, 999] и показывает наибольшую цифру числа.
---
+++ Seminar4/Program.cs
@@ -0,0 +1,91 @@
+﻿//Напишите программу, которая принимает на вход число (А)
+// и выдаёт сумму чисел от 1 до А.
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Cумма цифр = {GetSumNums2(num)}");
+
+
+int GetSumNums1(int number)
+{
+    //1+1
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum;
+}
+
+int GetSumNums2(int number)
+{
+    int result = 0;
+
+    for (int i = 1; i <= number; i++)
+    {
+        result += i;
+    }
+
+    return result;
+
+}
+/*конец*/
+
+//Напишите программу, которая принимает 
+//на вход число N и выдаёт количество цифр в числе.
+
+Console.Write("Введите число: ");
+num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Количество цифр = {GetCountNums(num)}");
+
+
+int GetCountNums(int number)
+{
+    int count = 0;
+    while (number > 0)
+    {
+        count++;
+        number /= 10;
+    }
+    return count;
+}
+/*конец*/
+
+//Напишите программу, которая принимает 
+//на вход число N и выдаёт произведение чисел от 1 до N.
+
+Console.Write("Введите число: ");
+num = int.Parse(Console.ReadLine());
+Console.WriteLine($"Произведение цифр = {GetMultiply(num)}");
+
+
+int GetMultiply(int number)
+{
+    int result = 1;
+    for (int i = 2; i <= number; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
+/*конец*/
+
+//Напишите программу, которая выводит массив из 8 элементов,
+//заполненный нулями и единицами в случайном порядке.
+int[] array = GetBinaryArray(8);
+Console.WriteLine($"[{String.Join(",", array)}]");
+
+
+int[] GetBinaryArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(2);
+    }
+
+    return result;
+}
+/*конец*/
